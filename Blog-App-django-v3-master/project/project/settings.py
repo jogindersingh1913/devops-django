d54a3ea295625d7afc485663bc769ca889879024
x22129588 <jogindersingh.ramani1@gmail.com>
2023-04-12 15:35:54 +0100

    nci backgroug image added

--- Blog-App-django-v3-master/project/project/settings.py
+++ Blog-App-django-v3-master/project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+_y5p=oj2uk#a1!k*4rlcm)01@s@ud1_er%kenn^@)p!^b6-u3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 LOGIN_REDIRECT_URL = 'blog_list'   # requires an html file (login.html).
 
 LOGOUT_REDIRECT_URL = 'blog_list'  # doesn't require any html file.
