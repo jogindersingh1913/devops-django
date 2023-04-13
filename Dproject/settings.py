984d55bb24e61303c6b8a0958663eb8c012e49c0
x22129588 <jogindersingh.ramani1@gmail.com>
2023-04-14 00:38:08 +0100

    added allowed host

--- Dproject/settings.py
+++ Dproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-126)eb0au&=pvk=2=56r08xv#-xi$1mi(khml!bu@*croof+qu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
